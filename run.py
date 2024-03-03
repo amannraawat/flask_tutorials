a4ad38cf5f1e0e286e8da62365c12251d29c57b0
Aman Rawat <amannraawat@gmail.com>
2024-03-03 17:09:59 +0530

    modify the website into a blog website

--- run.py
+++ run.py
@@ -3,13 +3,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return render_template('home.html')
+def home():
+    return render_template('index.html')
 
 @app.route("/about")
 def about():
-    nam = "About"
-    return render_template('about.html', name=nam)
+    return render_template('about.html')
+
+@app.route("/contact")
+def contact():
+    render_template('contact.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
