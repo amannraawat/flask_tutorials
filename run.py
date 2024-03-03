20d92a896e4ceb6a5618e54b3c0c41e70aad952d
Aman Rawat <amannraawat@gmail.com>
2024-03-03 13:46:53 +0530

    style home and about page

--- run.py
+++ run.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template('home.html')
+
+@app.route("/about")
+def about():
+    nam = "About"
+    return render_template('about.html', name=nam)
 
 if __name__ == '__main__':
     app.run(debug=True)
