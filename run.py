73da9fca0f1172eb370278eabaff37c5b80ba08c
Aman Rawat <amannraawat@gmail.com>
2024-03-08 12:06:13 +0530

    implement pagination in our flask for a better experience

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import json
 from flask_mail import Mail
-import os
+import os, math
 from werkzeug.utils import secure_filename
 
 with open('config.json', 'r') as file:
@@ -51,8 +51,27 @@ class Posts(db.Model):
     
 @app.route("/")
 def home():
-    posts = Posts.query.filter_by().all()[0:params['no_of_posts']]
-    return render_template('index.html', params=params, posts=posts)
+    posts = Posts.query.filter_by().all()
+    last = math.ceil(len(posts)/int(params['no_of_posts']))
+    
+    page = request.args.get('page')
+    if (not str(page).isnumeric()):
+        page = 1
+    page = int(page)
+    posts = posts[(page-1) * int(params['no_of_posts']) : (page-1)*int(params['no_of_posts']) + int(params['no_of_posts'])]
+    
+    #pagination
+    if (page==1):
+        prev = '#'
+        next = "/?page=" + str(page+1)
+    elif (page==last):
+        prev = "/?page=" + str(page-1)
+        next = '#'
+    else:
+        prev = "/?page=" + str(page-1)
+        next = "/?page=" + str(page+1)
+        
+    return render_template('index.html', params=params, posts=posts, prev=prev, next=next)
 
 @app.route("/about")
 def about():
